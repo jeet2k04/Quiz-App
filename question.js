1dedffa6dd7f7df32961c2e2e11eeb9f4a485efa
Biswajit Pradhan <biswajitpradhan838@gmail.com>
2024-07-30 10:16:57 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,94 @@
+
+let questions = [
+  {
+    numb: 1,
+    question: "Javascript is a which type of language ?",
+    answer: "Object Oriented",
+    options: [
+      "Object Oriented",
+      "Structure Oriented",
+      "Interepted",
+      "Lowerlevel"
+    ]
+  },
+
+  {
+    numb: 2,
+    question: "Which of the following is a server-side Java Script object?",
+    answer: "File",
+    options: [
+      "Function",
+      "File",
+      "File Upload",
+      "Date"
+    ]
+  },
+    {
+    numb: 3,
+    question: "JavaScript ignores extra spaces",
+    answer: "False",
+    options: [
+      "True",
+      "False",
+      "Sometimes",
+      "everytime"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the alternate name for Java script?",
+    answer: "ECMAScript",
+    options: [
+      "ECMAScript",
+      "ECMScript",
+      " Both a and d",
+      "LimeScript"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is a client-side Java Script object?",
+    answer: "FileUpload",
+    options: [
+      "File",
+      "Function",
+      "Time",
+      "FileUpload"
+    ]
+  },
+    {
+    numb: 6,
+    question: " Java Script entities start with ____________ and end with ______________",
+    answer: "Ampersand, semicolon",
+    options: [
+      "Semicolon, colon",
+      "Semicolon, Ampersand",
+      "Ampersand, semicolon",
+      "Ampersand, colon"
+    ]
+  },
+
+    {
+    numb: 7,
+    question: "What does API stand for?",
+    answer: "Application Programming Interface",
+    options: [
+      "Applicate Preprogramming Interface",
+      "Application Preprocessor Interface",
+      "Application Programming Interface",
+      "Application Programming Id"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "What does CDN stand for?",
+    answer: "Content Delivery Network",
+    options: [
+      "Content Delivery Network",
+      "Create Delivery Network",
+      "Cascading Development Network",
+      "Content Delivery Natation"
+    ]
+  },
+];
